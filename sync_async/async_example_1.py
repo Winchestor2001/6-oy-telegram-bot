b29dfc65cfb3ea47a898f21b84c9d823c835f651
winchestor2001 <bexruz2001xx@gmail.com>
2023-01-17 15:33:07 +0500

    lesson_1

---
+++ sync_async/async_example_1.py
@@ -0,0 +1,30 @@
+import asyncio
+import time
+
+
+async def sleep1():
+    print(f'Time: {time.time() - start:.2f}')
+    time.sleep(1)
+
+
+async def sum1(name, numbers):
+    total = 0
+    for number in numbers:
+        print(f'Task {name}: Computing {total}+{number}')
+        await sleep1()
+        total += number
+    print(f'Task {name}: Sum = {total}\n')
+
+
+start = time.time()
+
+loop = asyncio.get_event_loop()
+tasks = [
+    loop.create_task(sum1("A", [1, 2])),
+    loop.create_task(sum1("B", [1, 2, 3])),
+]
+loop.run_until_complete(asyncio.wait(tasks))
+loop.close()
+
+end = time.time()
+print(f'Time: {end - start:.2f} sec')
