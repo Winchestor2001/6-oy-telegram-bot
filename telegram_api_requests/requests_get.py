f3741a00045b1f0bfce4ecc6346ce70e2751bb3a
winchestor2001 <bexruz2001xx@gmail.com>
2023-01-19 16:00:14 +0500

    lesson_1

---
+++ telegram_api_requests/requests_get.py
@@ -0,0 +1,33 @@
+from pprint import pprint
+
+import requests
+
+# GET, POST, PUT
+# r = requests.get("https://api.github.com/events")
+# print(r.status_code)
+# print(dir(r))
+# data = r.json()
+# print(data[0]['actor']['display_login'])
+# print(type(r.text))
+
+
+token = '5674925771:AAE-McW8QREcJ90gSsv2ivWPZEPDr2oEbEM'
+my_id = 591250245
+get_me = requests.get(f'https://api.telegram.org/bot{token}/getMe')
+get_updates = requests.get(f'https://api.telegram.org/bot{token}/getUpdates')
+post_message = requests.get(f'https://api.telegram.org/bot{token}/sendMessage?chat_id={my_id}')
+# print(get_me.status_code)
+# print(get_updates.status_code)
+# pprint(get_me.json())
+# pprint(get_updates.json())
+
+
+
+
+
+
+
+
+
+
+
