b462369edaed5fc920cc8912d3f324db883b22b7
winchestor2001 <bexruz2001xx@gmail.com>
2023-01-21 15:16:50 +0500

    lesson_1

--- telegram_api_requests/requests_get.py
+++ telegram_api_requests/requests_get.py
@@ -15,7 +15,11 @@ token = '5674925771:AAE-McW8QREcJ90gSsv2ivWPZEPDr2oEbEM'
 my_id = 591250245
 get_me = requests.get(f'https://api.telegram.org/bot{token}/getMe')
 get_updates = requests.get(f'https://api.telegram.org/bot{token}/getUpdates')
-post_message = requests.get(f'https://api.telegram.org/bot{token}/sendMessage?chat_id={my_id}')
+post_message = requests.post(
+    f'https://api.telegram.org/bot{token}/sendMessage',
+    data={'chat_id': my_id, 'text': 'Salom'}
+)
+print(post_message)
 # print(get_me.status_code)
 # print(get_updates.status_code)
 # pprint(get_me.json())
