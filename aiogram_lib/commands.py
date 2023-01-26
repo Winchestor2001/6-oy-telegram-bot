25be15ba7adb0d8df50a994827bf64c5fcaffba2
winchestor2001 <bexruz2001xx@gmail.com>
2023-01-26 14:41:45 +0500

    lesson_1

--- aiogram_lib/commands.py
+++ aiogram_lib/commands.py
@@ -7,6 +7,7 @@ BOT_TOKEN = '5674925771:AAE-McW8QREcJ90gSsv2ivWPZEPDr2oEbEM'
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
+count = 1
 
 
 async def set_commands(dp: Dispatcher):
@@ -21,6 +22,25 @@ async def set_commands(dp: Dispatcher):
 @dp.message_handler(commands=['start'])
 async def start_bot(message: types.Message):
     await message.answer("Salom")
+
+
+@dp.message_handler(commands=['help'])
+async def help_bot(message: types.Message):
+    context = f"""
+            Bot commands:\n\n
+    /start = Botni ishga tushurish\n
+    /count = Commandani ishlatilgan soni\n
+    /sticker = Sticker jo'natish\n
+    /emoji = Emoji jo'natish
+            """
+    await message.answer(context)
+
+
+@dp.message_handler(commands=['count'])
+async def counter_handler(message: types.Message):
+    global count
+    await message.answer(f"Click: {count}")
+    count += 1
 
 
 if __name__ == '__main__':
