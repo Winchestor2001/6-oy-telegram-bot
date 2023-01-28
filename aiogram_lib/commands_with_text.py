d4a86c9ac0d3503557d5bfe1ed70c73c53107281
winchestor2001 <bexruz2001xx@gmail.com>
2023-01-28 14:44:17 +0500

    lesson_3

--- aiogram_lib/commands_with_text.py
+++ aiogram_lib/commands_with_text.py
@@ -38,5 +38,39 @@ async def help_bot(message: types.Message):
     await message.answer(context)
 
 
+@dp.message_handler(regexp='count')
+async def counter_handler(message: types.Message):
+    global count
+    await message.answer(f"Click: {count}")
+    count += 1
+
+
+@dp.message_handler(content_types=['text'])
+async def any_handlers(message: types.Message):
+    text = message.text
+    if text == 'sticker':
+        await message.reply_sticker('CAACAgIAAxkBAAEHc9Vj0ktpZ661Pwp-qBFZnbag3YmAsAACNBMAAjGSoEvT_Q2YdUoyGS0E')
+    elif text == 'emoji':
+        emojies = ['😇', '🥸', '🤯', '🥶', '🤑']
+        await message.answer(f"Siz {choice(emojies)}")
+        # await message.delete()
+        await asyncio.sleep(5)
+        await bot.delete_message(message.chat.id, message.message_id + 1)
+    elif text == 'uzb':
+        context = f'Uzbekistan\n' \
+                  f'<b>Uzbekistan</b>\n' \
+                  f'<em>Uzbekistan</em>\n' \
+                  f'<u>Uzbekistan</u>\n' \
+                  f'<s>Uzbekistan</s>\n' \
+                  f'<tg-spoiler>Uzbekistan</tg-spoiler>\n' \
+                  f'<a href="https://youtu.be/vQVwkyn3-F8">Uzbekistan</a>\n' \
+                  f'<code>Uzbekistan</code>\n' \
+                  f'<pre>Uzbekistan</pre>'
+        await message.answer_photo(photo='https://www.advantour.com/img/uzbekistan/symbolics/uzbekistan-flag.jpg',
+                                   caption=context)
+    else:
+        await message.reply("Sizni tushunmadim!")
+
+
 if __name__ == '__main__':
     executor.start_polling(dp, on_startup=set_commands, skip_updates=True)
