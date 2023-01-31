9c91a805d07d0a3f62530a4d9f067ee2823edb0f
winchestor2001 <bexruz2001xx@gmail.com>
2023-01-31 15:14:12 +0500

    lesson_4

--- aiogram_lib/big_keyboard.py
+++ aiogram_lib/big_keyboard.py
@@ -12,6 +12,17 @@ dp = Dispatcher(bot)
 start_menu_btn = types.ReplyKeyboardMarkup(resize_keyboard=True)
 start_menu_btn.row("Biz Xaqimizda", "Contact")
 
+about_btn = types.ReplyKeyboardMarkup(resize_keyboard=True)
+about_btn.row("Video konsultatsiya")
+about_btn.row("Ortga")
+
+contact_btn = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
+contact_btn.add(
+    types.KeyboardButton("Location"),
+    types.KeyboardButton("Telefon raqamlar"),
+    types.KeyboardButton("Ortga"),
+)
+
 
 async def set_commands(dp: Dispatcher):
     await dp.bot.set_my_commands(
@@ -25,6 +36,25 @@ async def set_commands(dp: Dispatcher):
 @dp.message_handler(commands=['start'])
 async def start_bot(message: types.Message):
     await message.answer("Assalomu aleykum", reply_markup=start_menu_btn)
+
+
+@dp.message_handler(content_types=['text'])
+async def any_btn_handler(message: types.Message):
+    text = message.text
+
+    if text == 'Biz Xaqimizda':
+        await message.answer("Biz Nits Python guruximiz!", reply_markup=about_btn)
+
+    elif text == 'Contact':
+        await message.answer("Biz bilan zudlikbilan bog'laning!", reply_markup=contact_btn)
+
+    elif text == 'Ortga':
+        # await start_bot(message)
+        await message.answer("Assalomu aleykum", reply_markup=start_menu_btn)
+
+    elif text == 'Video konsultatsiya':
+        video = types.InputFile('video.mp4')
+        await message.answer_video(video, width=400, height=60)
 
 
 if __name__ == '__main__':
