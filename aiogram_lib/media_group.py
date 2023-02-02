7dc14d231fecd48ed5ec97d8970bc4282e69e014
winchestor2001 <bexruz2001xx@gmail.com>
2023-02-02 15:04:35 +0500

    lesson_5

--- aiogram_lib/media_group.py
+++ aiogram_lib/media_group.py
@@ -21,15 +21,18 @@ async def set_commands(dp: Dispatcher):
 
 @dp.message_handler(commands=['start'])
 async def start_bot(message: types.Message):
+    await message.answer_chat_action('typing')
+    # await message.answer_chat_action('choose_sticker')
+    # await message.answer_chat_action('find_location')
     await message.answer("Assalomu aleykum")
 
 
 @dp.message_handler(commands=['media'])
 async def send_medies_handler(message: types.Message):
     media = types.MediaGroup()
-    media.attach_photo(types.InputFile('img/6. Жираф.jpg'))
+    media.attach_photo(types.InputFile('img/6. Жираф.jpg'), caption="Rasim")
     media.attach_photo(types.InputFile('img/10. Цветы.jpg'))
-    media.attach_video(types.InputFile('video/video.mp4'))
+    # media.attach_video(types.InputFile('video/video.mp4'))
     await message.answer_chat_action('upload_photo')
     # actions: upload_photo, upload_video, upload_audio, record_voice, typing, record_video,
     # upload_voice, upload_document, find_location, choose_sticker
