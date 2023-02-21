e05d24cf16200b05d1b95b182e259482366ea0e1
winchestor2001 <bexruz2001xx@gmail.com>
2023-02-21 14:29:54 +0500

    lesson_1

--- PhotoEffectsBot/bot.py
+++ PhotoEffectsBot/bot.py
@@ -1,10 +1,16 @@
 from aiogram import Bot, Dispatcher, types
 import logging
 
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
 logging.basicConfig(level=logging.INFO)
 
 BOT_TOKEN = ''
 
 bot = Bot(token=BOT_TOKEN, parse_mode=types.ParseMode.HTML)
-dp = Dispatcher(bot)
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
+
+
+
 
