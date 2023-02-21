ad189e9b92466714b055c923f0cbe112cc5faf89
winchestor2001 <bexruz2001xx@gmail.com>
2023-02-21 14:28:11 +0500

    lesson_1

--- PhotoEffectsBot/bot.py
+++ PhotoEffectsBot/bot.py
@@ -1,4 +1,10 @@
 from aiogram import Bot, Dispatcher, types
 import logging
 
+logging.basicConfig(level=logging.INFO)
+
 BOT_TOKEN = ''
+
+bot = Bot(token=BOT_TOKEN, parse_mode=types.ParseMode.HTML)
+dp = Dispatcher(bot)
+
