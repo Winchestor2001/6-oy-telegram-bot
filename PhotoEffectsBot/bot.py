5beb01cb3b0be36c17c5c3ab3e543574d6c06829
winchestor2001 <bexruz2001xx@gmail.com>
2023-02-21 14:31:54 +0500

    lesson_1

--- PhotoEffectsBot/bot.py
+++ PhotoEffectsBot/bot.py
@@ -1,16 +1,21 @@
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher, types, executor
 import logging
 
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 logging.basicConfig(level=logging.INFO)
 
-BOT_TOKEN = ''
+BOT_TOKEN = '5674925771:AAE-McW8QREcJ90gSsv2ivWPZEPDr2oEbEM'
 
 bot = Bot(token=BOT_TOKEN, parse_mode=types.ParseMode.HTML)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
 
 
+@dp.message_handler(commands=['start'])
+async def bot_start_handler(message: types.Message):
+    await message.answer("Assalomu aleykum")
 
 
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
